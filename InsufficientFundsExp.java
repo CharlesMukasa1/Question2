ba5e90fae0a30006825efbe96c939b3c052d49fd
CharlesMukasa1 <ckaslex@gmail.com>
2026-07-03 15:36:59 +0300

    Add files via upload

---
+++ InsufficientFundsExp.java
@@ -0,0 +1,10 @@
+
+package isaacwandyakaortis.qn2exceptions;
+
+public class InsufficientFundsExp extends Exception {
+     
+    public InsufficientFundsExp(String message) {
+        super(message);
+    }
+
+}
